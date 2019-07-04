7800351da1b7086c79a7d068ba0e7c1f26ac0a29
baviamu <48151961+baviamu@users.noreply.github.com>
2019-07-04 11:38:16 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,8 +1,5 @@
-num = int(input("Enter any number: "))
-flag = num%2
-if flag == 0:
-    print(num, "is an even number")
-elif flag == 1:
-    print(num, "is an odd number")
-else:
-    print("Error, Invalid input")
+a=int(input("Please Enter a Number : "));  
+if(a&1==1):  
+    print("This Number is Odd")    
+else:  
+    print("This Number is Even")
